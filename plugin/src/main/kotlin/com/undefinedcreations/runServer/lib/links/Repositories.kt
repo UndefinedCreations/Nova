621b9be67ae5f01f1c6ca876b1df171fce630856
StillLutto <still.lutto@gmail.com>
2025-04-18 11:18:50 +0200

    fix(repositories): spelling mistakes

--- plugin/src/main/kotlin/com/undefinedcreations/runServer/lib/links/Repositories.kt
+++ plugin/src/main/kotlin/com/undefinedcreations/runServer/lib/links/Repositories.kt
@@ -11,18 +11,22 @@ object Repositories {
      * The PaperMC repository to download papermc, velocity, folia, waterfall
      */
     const val PAPERMC_REPO = "https://api.papermc.io/v2/projects"
+
     /**
      * The UndefinedCreations repository to download spigotmc
      */
-    const val UNDEFINEDCREATION_REPO = "https://repo.undefinedcreations.com/server/spigotmc"
+    const val UNDEFINEDCREATIONS_REPO = "https://repo.undefinedcreations.com/server/spigotmc"
+
     /**
      * The md5 repository to download bungeecord
      */
     const val BUNGEECORD_REPO = "https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar"
+
     /**
      * The Purper repository to download purper
      */
-    const val PURPER_REPO = "https://api.purpurmc.org/v2/purpur"
+    const val PURPUR_REPO = "https://api.purpurmc.org/v2/purpur"
+
     /**
      * The PufferFish repository to download pufferfish
      */
@@ -32,10 +36,12 @@ object Repositories {
      * The spigotmc repository to download plugins from spigotmc.org
      */
     const val SPIGOT_API = "https://api.spiget.org/v2/resources/"
+
     /**
      * The papermc hanger repository to download plugins from hangar.papermc.io
      */
     const val PAPERMC_API = "https://hangar.papermc.io/api/v1/projects/"
+
     /**
      * The modrinth repository to download plugins from modrinth.com
      */
